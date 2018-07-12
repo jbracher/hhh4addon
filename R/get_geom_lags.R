6c469dadda52ef882a18f80e3c46f16dda73671e
Johannes <jbracher@outlook.de>
2018-07-12 14:03:08 +0200

    Added other lag functions (Poisson and ar2)

--- R/get_geom_lags.R
+++ R/get_geom_lags.R
@@ -60,3 +60,62 @@ geometric_lag <- function(lag1, par_lag, min_lag, max_lag, sum_up = TRUE){ #BJ
   } #BJ
   geom_lag #BJ
 } #BJ
+
+
+# helper function: transform matrix of first lags to matrix of geometric lags
+poisson_lag <- function(lag1, par_lag, min_lag, max_lag, sum_up = TRUE){ #BJ
+
+  if(is.list(par_lag)){
+    p_lag <- par_lag$mu # extract parameter
+  }else{
+    p_lag <- par_lag
+  }
+
+  weights0 <- c(rep(0, min_lag - 1), dpois((min_lag:max_lag) - 1, p_lag)) #BJ
+  weights <- weights0/sum(weights0) #BJ
+  pois_lag <- if(sum_up){
+    matrix(0, ncol = ncol(lag1), nrow = nrow(lag1))
+  }else{
+    array(NA, dim = c(dim(lag1), max_lag))
+  }
+
+  # first line contains NAs
+  for(i in 1:max_lag){ #BJ
+    lag_i <- lag1[c(rep(1, i - 1), 1:(nrow(pois_lag) - i + 1)), , drop = FALSE] #BJ
+    if(sum_up){
+      pois_lag <- pois_lag + weights[i]*lag_i #BJ
+    }else{
+      pois_lag[,,i] <- weights[i]*lag_i
+    }
+  } #BJ
+  pois_lag #BJ
+} #BJ
+
+# helper function: transform matrix of first lags to matrix of geometric lags
+ar2_lag <- function(lag1, par_lag, min_lag, max_lag, sum_up = TRUE){ #BJ
+
+  if(is.list(par_lag)){
+    p_lag <- par_lag$mu # extract parameter
+  }else{
+    p_lag <- par_lag
+  }
+
+  weights0 <- c(rep(0, min_lag - 1), p_lag, 1 - p_lag, rep(0, max_lag - min_lag - 1)) #BJ
+  weights <- weights0/sum(weights0) #BJ
+  ar2_lag <- if(sum_up){
+    matrix(0, ncol = ncol(lag1), nrow = nrow(lag1))
+  }else{
+    array(NA, dim = c(dim(lag1), max_lag))
+  }
+
+  # first line contains NAs
+  for(i in 1:max_lag){ #BJ
+    lag_i <- lag1[c(rep(1, i - 1), 1:(nrow(ar2_lag) - i + 1)), , drop = FALSE] #BJ
+    if(sum_up){
+      ar2_lag <- ar2_lag + weights[i]*lag_i #BJ
+    }else{
+      ar2_lag[,,i] <- weights[i]*lag_i
+    }
+  } #BJ
+  ar2_lag #BJ
+} #BJ
